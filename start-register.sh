4311d72c29348bf74a97ca8dca2e45c74f85fa0d
Activity Watch <pml@simpledata.ch>
2012-04-07 15:09:38 +0200

    adjusted register

--- start-register.sh
+++ start-register.sh
@@ -1,1 +1,1 @@
-authbind node source/app.js --http.port 80 --http.host 91.121.41.240 --dns.port 53 --dns.host 91.121.41.240 --dns.hostname ns1.wactiv.com
+authbind node source/app.js --http.port_static 80 --http.port_register 443 --register_ssl true --http.host 91.121.41.240 --dns.port 53 --dns.host 91.121.41.240 --dns.hostname ns1.wactiv.com
