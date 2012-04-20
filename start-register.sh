0c3b6629fa03dd2187bbc3a6e26a8b8f955a9245
Activity Watch <pml@simpledata.ch>
2012-04-20 18:21:52 +0200

    updated start script

--- start-register.sh
+++ start-register.sh
@@ -1,2 +1,2 @@
 cd source
-authbind node app.js --http.port_static 80 --http.port_register 443 --http.register_ssl true --http.host 91.121.41.240 --dns.port 53 --dns.host 91.121.41.240 --dns.hostname ns1.wactiv.com
+authbind node app.js --http.static.port 80 --http.register.port 443 --http.register.ssl true --http.static.host 91.121.41.240 --http.register.host 91.121.41.240 --dns.port 53 --dns.host 91.121.41.240 --dns.hostname ns1.wactiv.com
