70eca4e73da963f1b8e0bf6e4db3f2634c6ea580
Perki <pm@perki.com>
2012-04-21 14:05:31 +0200

    switched host to ip in start script

--- start-register.sh
+++ start-register.sh
@@ -1,5 +1,5 @@
 cd source
 authbind node app.js \
-    --http.static.port 80 --http.static.host 91.121.41.240  --http.static.name rec.la \
-    --http.register.port 443 --http.register.host 91.121.41.240  --http.register.name rec.la --http.register.ssl true \
-    --dns.port 53 --dns.host 91.121.41.240 --dns.hostname ns1.wactiv.com
+    --http.static.port 80 --http.static.ip 91.121.41.240  --http.static.name rec.la \
+    --http.register.port 443 --http.register.ip 91.121.41.240  --http.register.name rec.la --http.register.ssl true \
+    --dns.port 53 --dns.ip 91.121.41.240 --dns.hostname ns1.wactiv.com
