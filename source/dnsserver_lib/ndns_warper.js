7fbaa011a2a6376db3a91d382b3749edbf7e09d9
Perki <pm@perki.com>
2012-07-02 17:30:14 +0200

    added some debug lines to dns server

--- source/dnsserver_lib/ndns_warper.js
+++ source/dnsserver_lib/ndns_warper.js
@@ -69,6 +69,11 @@ exports.start = function(NAMES,BIND_PORT,BIND_HOST,dynamic_call,done) {
     
     if (req.q.length > 0) {
       var name = req.q[0].name;
+      if (name == undefined) {
+        console.debug("How comes DNS request was null ??"+JSON.stringify(req));
+        name = "";
+      }
+      console.log(JSON.stringify(req));
       if (name == ".") name = "";
       
       middle_send_response = function (req,res,rec) {
