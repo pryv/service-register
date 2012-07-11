e7c0b040dc72cc40e9c49c815b8dc8345820aae1
Simon Goumaz <simon@attentif.ch>
2012-07-11 16:16:31 +0200

    Quick scripts cleanup

--- start-dns2.sh
+++ start-dns2.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 cd source
 authbind node app_dns.js --dns.ip 91.121.41.94 --dns.hostname ns2.wactiv.com
--- start-register.sh
+++ start-register.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 cd source
 authbind node app.js --http.register.ip 91.121.41.240 --dns.ip 91.121.41.240 --http.static.name d2p322ssjiukh3.cloudfront.net
