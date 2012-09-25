9e32a18a11ddca04160b669cee705eabe21a0cbd
Perki <pm@perki.com>
2012-09-25 18:21:59 +0200

    new spawn database script

--- scripts/fleet-spawn-database.bash
+++ scripts/fleet-spawn-database.bash
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+. ~/.profile
+
+# working dir fix
+scriptsFolder=$(cd $(dirname "$0"); pwd)
+cd $scriptsFolder/
+. ./env-config.sh
 
 droneId=$1
 
 [ -z "$droneId" ] && echo "Expected argument: <fleet drone id>" && exit 1
 
 fleet spawn --drone=$droneId --repo=registration-server -- \
-/home/wactiv/redis-2.4.8/src/redis-server ./db/s1.conf
+/$redisName/src/redis-server ./db/master.conf
