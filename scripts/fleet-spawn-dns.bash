e1c8d04321cb4a13d34071e01569c25c41bebfff
Perki <pm@perki.com>
2012-10-03 17:26:15 +0200

    updated spawn dns script

--- scripts/fleet-spawn-dns.bash
+++ scripts/fleet-spawn-dns.bash
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+. ~/.profile
 
 droneId=$1
 
 [ -z "$droneId" ] && echo "Expected argument: <fleet drone id>" && exit 1
 
+
 fleet spawn --drone=$droneId --repo=registration-server -- \
-"authbind node source/app_dns.js --dns.ip 91.121.41.94 --dns.hostname ns2.wactiv.com >> /home/wactiv/logs/dns.log 2>&1"
+"node source/app_dns.js --config $2"
