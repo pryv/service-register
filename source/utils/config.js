c101bc5703cf53b1b01c2f3f4769880e669034a3
Perki <pm@perki.com>
2012-10-03 11:53:06 +0200

    Password database connetion

--- source/utils/config.js
+++ source/utils/config.js
@@ -33,60 +33,63 @@ nconf.file({ file: configFile});
 
 // Set default values
 nconf.defaults({
-  'languages': {
-      'default' : 'en',
-      'supported' : [{'en': 'English'}, {'fr': 'Français'}]
+  "languages": {
+      "default" : "en",
+      "supported" : [{"en": "English"}, {"fr": "Français"}]
   },
-  'http': {  // this should match the config of sww
-    'static': {
-      'port': 443,
-      'name': 'www.wactiv.com', // used by dns and index.js
-      'ssl': true,
-      'no_ssl_on_port': 80, // IF SSL IS ON also listen to this port 0 if not
+  "http": {  // this should match the config of sww
+    "static": {
+      "port": 443,
+      "name": "www.wactiv.com", // used by dns and index.js
+      "ssl": true,
+      "no_ssl_on_port": 80, // IF SSL IS ON also listen to this port 0 if not
     },
-    'register': {
-      'port': 443, 
-      //'ip': '127.0.0.1'
-      'name': 'rec.la',
-      'ssl': true, // turn ssl on
-      'no_ssl_on_port': 80, // IF SSL IS ON also listen to this port 0 if not
+    "register": {
+      "port": 443, 
+      //"ip": "127.0.0.1"
+      "name": "rec.la",
+      "ssl": true, // turn ssl on
+      "no_ssl_on_port": 80, // IF SSL IS ON also listen to this port 0 if not
     }
   },
-  'persistence' : { 
-    'init-ttl' : 86400 // seconds should be 86400 for a day
+  "persistence" : { 
+    "init-ttl" : 86400 // seconds should be 86400 for a day
   },
-  'net': { // manly used in /network/dataservers
-    'AAservers_domain': 'wactiv.com', // domaine for all admin / activity servers
-    'aaservers_ssl': true, // set if admin / activity servers have ssl
-    'aaservers': 
+  "net": { // manly used in /network/dataservers
+    "AAservers_domain": "wactiv.com", // domaine for all admin / activity servers
+    "aaservers_ssl": true, // set if admin / activity servers have ssl
+    "aaservers": 
       [{ "base_name": "test1", "port": 443, "authorization": "register-test-token" , ip: "91.121.34.251"}, 
        { "base_name": "test2", "port": 443, "authorization": "register-test-token" , ip: "46.105.35.181" }]
   },
-  'mailer': {
-    'deactivated' : false, // globally deactivate mailing
-    'confirm-sender-email': 'active@rec.la',
-    'amazon_ses' : {
-      'accesskeyid': 'AKIAIHR6HVRME43VNCSA',
-      'secretkey': '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',
-      'serviceurl': 'https://email.us-east-1.amazonaws.com'
+  "mailer": {
+    "deactivated" : false, // globally deactivate mailing
+    "confirm-sender-email": "active@rec.la",
+    "amazon_ses" : {
+      "accesskeyid": "AKIAIHR6HVRME43VNCSA",
+      "secretkey": "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",
+      "serviceurl": "https://email.us-east-1.amazonaws.com"
     }
   },
-  'dns': {
-      'port': 53,
-      //'ip': '127.0.0.1', // listen on a specific IP
-      'name': 'ns1.wactiv.com', // (my name for a dns) must be in nameserver list
-      'domain': 'rec.la',
-      'domain_A': '91.121.41.240', // should point to www
-      'default_ttl': 3600,
-   'nameserver': [{name: 'ns1.wactiv.com', ip: '91.121.41.240'},
-   				  {name: 'ns2.wactiv.com', ip: '91.121.41.94'}],
-   'mail' : [{name: "spool.mail.gandi.net", ip: "217.70.184.6", ttl: 10800, priority: 10 },
-             { name: "fb.mail.gandi.net", ip: "217.70.184.162", ttl: 10800, priority: 50 }]
+  "dns": {
+      "port": 53,
+      //"ip": "127.0.0.1", // listen on a specific IP
+      "name": "ns1.wactiv.com", // (my name for a dns) must be in nameserver list
+      "domain": "rec.la",
+      "domain_A": "91.121.41.240", // should point to www
+      "default_ttl": 3600,
+   "nameserver": [{"name": "ns1.wactiv.com", "ip": "91.121.41.240"},
+   				  {"name": "ns2.wactiv.com", "ip": "91.121.41.94"}],
+   "mail" : [{"name": "spool.mail.gandi.net", "ip": "217.70.184.6", "ttl": 10800, "priority": 10 },
+             { "name": "fb.mail.gandi.net", "ip": "217.70.184.162", "ttl": 10800, "priority": 50 }]
   },
-  'test': {
-    'init': {
-        'deactivate_mailer' : false,
-        'add_challenge' : true  // will add the challenge string to the response in order to chain tests
+  "redis": {
+    "password": "My recorded Life",
+  },
+  "test": {
+    "init": {
+        "deactivate_mailer" : false,
+        "add_challenge" : true  // will add the challenge string to the response in order to chain tests
     }
   }
 });
