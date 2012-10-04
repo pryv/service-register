9453d9ff67b64847443cb0f269d52a21320b4cd7
Perki <pm@perki.com>
2012-10-04 14:17:23 +0200

    updated spwan register

--- scripts/fleet-spawn-register.bash
+++ scripts/fleet-spawn-register.bash
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+
+. ~/.profile
+
+## config File path should be in destination path 
+## /home/register/reg-gandi-fr-01.pryv.net.json
 
 droneId=$1
+configFile=$2
 
 [ -z "$droneId" ] && echo "Expected argument: <fleet drone id>" && exit 1
 
