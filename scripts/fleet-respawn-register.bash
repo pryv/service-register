37417e51546633232b5cf110ba4d7ae9c7427642
Perki <pm@perki.com>
2012-10-09 22:10:20 +0200

    all in one script

---
+++ scripts/fleet-respawn-register.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+. ~/.profile
+
+## config File path should be in destination path 
+## /home/register/reg-gandi-fr-01.pryv.net.json
+
+droneId=$1
+tokill=$2
+configFile=$3
+
+[ -z "$configFile" ] && echo "Expected argument: <fleet drone id> <id to stop> <config file>" && exit 1
+
+fleet deploy --drone=$droneId
+./fleet-setup.bash $droneId
+fleet stop $tokill
+./fleet-spawn-register.bash $droneId $configFile
