3b6f8cc0f155c44f28fa24bd806a1b7fa8bb575e
Jon Maim <jonathan.maim@gmail.com>
2013-01-22 16:40:16 +0530

    puppet cleaning and adding upstart conf

--- scripts/puppet/manifests/init.pp
+++ scripts/puppet/manifests/init.pp
@@ -1,4 +1,4 @@
-class reg($nodeversion, $redisversion) {
+class reg($nodeversion, $redisversion, $app) {
   # package 
   package {
     'tcl8.5': ensure => installed;
@@ -15,5 +15,16 @@ class reg($nodeversion, $redisversion) {
   }
   class{'nodesetup':
     nodeversion => $nodeversion,
+  }
+  exec{'supervisor':
+    command => "npm install -g supervisor",
+    require => Class["nodesetup"],
+  }
+  file {"upstart":
+    path    => "/etc/init/app.conf",
+    ensure  => 'file',
+    content => template('reg/app.conf.erb'),
+    mode    => '644',
+    require => Exec["supervisor"],
   }
 }
