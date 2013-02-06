e7881305fcc9a8f55b5072d6379a98aa05ce4477
Perki <pm@perki.com>
2013-02-06 14:17:56 +0100

    alined registration-server to refactoring made for static-web

--- scripts/puppet/manifests/init.pp
+++ scripts/puppet/manifests/init.pp
@@ -1,40 +1,85 @@
-class reg($nodeversion, $redisversion, $app, $datadiskname, $conffile) {
-  notify{"reg datadiskname: $datadiskname conffile: $conffile":}
+class reg($app, $slaveof) {
+  
+  
+  
+  notify{"reg $app slaveof: $slaveof ":}
+  
   # package 
   package {
     'tcl8.5': ensure => installed;
   }
-  # live dir
-  file {'/var/www':
-    ensure  => 'directory',
-    mode    => '0644',
-  }
-  # app conf file
-  file {'/var/www/config.json':
-    ensure  => 'file',
-    mode    => '0644',
-    source  => "puppet:///modules/reg/var/www/app/source/$conffile.json",
-    require => File['/var/www'],
-  }
-  # install node for user root
-  #
-  class{'redissetup':
+  
+  
+  # used by upstart template
+  $appcode = "reg"   
+  $appdir = "$::livedir/$app"
+  $configdest = "$appdir.conf.json"
+  
+  $nodeversion = 'v0.8.2'
+  $redisversion = '2.4.16'
+  
+   # dependencies
+  class{'nodesetup': nodeversion => $nodeversion, }
+  class{'redis': 
     redisversion  => $redisversion,
-    datadiskname  => $datadiskname,
-    conffile      => $conffile,
+    slaveof       => $slaveof,
   }
-  class{'nodesetup':
-    nodeversion => $nodeversion,
-  }
+
   exec{'supervisor':
     command => "npm install -g supervisor",
     require => Class["nodesetup"],
   }
-  file {"upstart":
-    path    => "/etc/init/app.conf",
+  
+  
+  # app dir
+  file {"$appdir":
+    ensure  => 'directory',
+    mode    => '0644',
+    require => File["$::livedir"],
+  }
+
+  
+  if ($app == "staging-registration-server") {
+     $nodeenv = "developement"
+     $staging = ".staging"
+     secret::ssl{"rec.la": }
+     $httpcerts = "$::livedir/secret/rec.la"
+  } else {
+     $nodeenv = "production"
+     $staging = ""
+     secret::ssl{"pryv.io": }
+     $httpcerts = "$::livedir/secret/pryv.io" 
+  } 
+  
+  if ($slaveof) {
+    $configname = "dnsonly"
+  } else {
+    $configname = "master"
+  }
+
+  # app conf file
+  file {'app config file':
+    path    => $configdest,
     ensure  => 'file',
-    content => template("reg/$conffile.conf.erb"),
+    mode    => '0644',
+    content => template("reg/$configname${staging}.config.json.erb"),
+    require => File["$appdir"],
+  }
+  
+   
+  file {"reg setup script":
+    path    =>  "$appdir.setup.bash",
+    ensure  => 'file',
+    content => template("reg/setup-app.erb"),
+    mode    => '755',
+    require => File["$appdir"],
+  }
+   
+  file {"upstart":
+    path    =>  "/etc/init/reg.conf",
+    ensure  => 'file',
+    content => template("head/upstart-default-nodeapp.conf.erb"),
     mode    => '644',
-    require => Exec["supervisor"],
+    require => [Exec["supervisor"], File["app config file"]],
   }
 }
