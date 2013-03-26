67ca8439068d0371b7b7d6a0b3a153692dc9e2ff
perki <pm@perki.com>
2013-03-26 18:47:21 +0100

    server can be turned off with port = 0 (needed for puppet deployment)

--- source/server.js
+++ source/server.js
@@ -12,32 +12,38 @@ _ = require('underscore.string');
 var ready = require('readyness');
 ready.setLogger(logger.info);
 
+
 //https server
 logger.info('Register  server :'+config.get('http:register:url'));
 logger.info('Static  server :'+config.get('http:static:url'));
 
-var serverOptions = {
+
+if (config.get('server:port') > 0) {
+
+  var serverOptions = {
     key : fs.readFileSync(config.get('server:certsPathAndKey')+'-key.pem').toString(),
     cert : fs.readFileSync(config.get('server:certsPathAndKey')+'-cert.crt').toString(),
     ca : fs.readFileSync(config.get('server:certsPathAndKey')+'-ca.pem').toString(),
-};
+  };
 
 
-var  server = require('https').createServer(serverOptions, app);
-
-var appListening = ready.waitFor('register:listening:'+config.get('server:ip')+':'+config.get('server:port'));
-server.listen(config.get('server:port'),config.get('server:ip'), function() {
-  var address = server.address();
-  var protocol = server.key ? 'https' : 'http';
-  appListening(_.sprintf('SW %s://%s:%d in %s mode with ssl certs: '+config.get('server:certsPathAndKey'),
+  var server = require('https').createServer(serverOptions, app);
+  var appListening = ready.waitFor('register:listening:'+config.get('server:ip')+':'+config.get('server:port'));
+  server.listen(config.get('server:port'),config.get('server:ip'), function() {
+    var address = server.address();
+    var protocol = server.key ? 'https' : 'http';
+    appListening(_.sprintf('SW %s://%s:%d in %s mode with ssl certs: '+config.get('server:certsPathAndKey'),
       protocol, address.address, address.port, app.settings.env));
-}).on('error',function (e) {  
-  if (e.code == 'EACCES') {
-    logger.error('Cannot '+e.syscall+' on: '+ip+':'+port); 
-    throw(e);
-  }});
+  }).on('error',function (e) {
+      if (e.code == 'EACCES') {
+        logger.error('Cannot '+e.syscall+' on: '+ip+':'+port);
+        throw(e);
+      }});
 
-module.exports = server;
+  module.exports = server;
+} else {
+  logger.info('** Https server is off !');
+}
 //start dns
 require('./app-dns');
 
