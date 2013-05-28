ef9a14e32e342751cfb4d9c4fdae6f936a969d3a
perki <pm@perki.com>
2013-05-28 21:01:40 +0200

    refactored JSHINT access

--- source/routes/access.js
+++ source/routes/access.js
@@ -5,42 +5,43 @@ var messages = require('../utils/messages.js');
 var appErrors = require('../utils/app-errors.js');
 var config = require('../utils/config');
 var randGenerator = require('../utils/random');
-var dataservers = require('../network/dataservers.js')
+var dataservers = require('../network/dataservers.js');
 
 var domain = config.get('dns:domain');
 var activityServerPort = config.get('net:aaservers_ssl') ? 443 : 80;
 
 function access(app) {
-  
-  function _setAccessState(res,next,key,accessState) {
-    db.setAccessState(key, accessState, function(error, result) {
-      if (error) { return next(messages.ei()) ; }
-      //require('../utils/dump.js').inspect(accessState,result);
-      return res.json(accessState,accessState.code);
-    }); 
+
+  function _setAccessState(res, next, key, accessState) {
+    db.setAccessState(key, accessState, function (error) {
+      if (error) { return next(messages.ei()); }
+      //require('../utils/dump.js').inspect(accessState, result);
+      return res.json(accessState, accessState.code);
+    });
   }
-  
-  
+
+
   /**
    * request an access
    */
-  app.post('/access', function(req, res,next){
+  app.post('/access', function (req, res, next) {
     //--- parameters --//
     var requestingAppId = checkAndConstraints.appID(req.body.requestingAppId);
     if (! requestingAppId) {
-      return next(messages.e(400,'INVALID_APP_ID'));
+      return next(messages.e(400, 'INVALID_APP_ID'));
     }
 
     var requestedPermissions = checkAndConstraints.access(req.body.requestedPermissions);
     if (! requestedPermissions) {
-      return next(messages.e(400,'INVALID_DATA',{detail: 'Missing or invalid requestedPermissions field'}));
+      return next(messages.e(400, 'INVALID_DATA',
+        {detail: 'Missing or invalid requestedPermissions field'}));
     }
 
     var lang = checkAndConstraints.lang(req.body.languageCode);
 
     //-- TODO Complete Check URL validity
-    if (typeof (req.body.returnURL) === "undefined") {
-      return next(messages.e(400,'INVALID_DATA',{detail: 'Missing Return Url field'}));
+    if (typeof (req.body.returnURL) === 'undefined') {
+      return next(messages.e(400, 'INVALID_DATA', {detail: 'Missing Return Url field'}));
     }
     var returnURL = req.body.returnURL;
 
@@ -55,162 +56,163 @@ function access(app) {
     //--- END CHECK APP AUTH ---//
 
     /**
-     * appname: "a name for the app",
-     * access: "the required access",
+     * appname: 'a name for the app',
+     * access: 'the required access',
      */
-    
+
     // is this a returning user (look in cookies)
-    if ( req.cookies.accessKey ) {
-      require('../utils/dump.js').inspect( { cookie: req.cookies});
-      checkKeyAndGetValue(req,res,next,function(key,value) { 
-        
-        return res.json(value,value.code);
+    if (req.cookies.accessKey) {
+      require('../utils/dump.js').inspect({ cookie: req.cookies});
+      checkKeyAndGetValue(req, res, next, function (key, value) {
+
+        return res.json(value, value.code);
       });
     }
-    
+
     // .... do some stuff here
 
 
     // step 2 .. register or log in
     var error = false;
     if (error) {
-      return next(messages.ei()) ; 
+      return next(messages.ei());
     }
-    
-    
-     
-    
-    
+
+
+
+
+
     var key = randGenerator.string(16);
-    var pollURL = config.get('http:register:url')+'/access/'+key;
-    
+    var pollURL = config.get('http:register:url') + '/access/' + key;
+
     var url = config.get('http:static:access');
- 
-    if (typeof req.body.localDevel !== "undefined") {
-       url = config.get('devel:static:access')+req.body.localDevel;
+
+    if (typeof req.body.localDevel !== 'undefined') {
+      url = config.get('devel:static:access') + req.body.localDevel;
     }
-    
-    
+
+
     url = url +
-    '?lang='+lang+
-    '&key='+key+
-    '&requestingAppId='+requestingAppId+
-    '&returnURL='+encodeURIComponent(returnURL)+
-    '&domain='+domain+
-    '&registerURL='+encodeURIComponent(config.get('http:register:url'));
-    
+      '?lang=' + lang +
+      '&key=' + key +
+      '&requestingAppId=' + requestingAppId +
+      '&returnURL=' + encodeURIComponent(returnURL) +
+      '&domain=' + domain +
+      '&registerURL=' + encodeURIComponent(config.get('http:register:url'));
+
     //TODO add username & sessionID if possible
-    
-    
-    var accessURIc = '&requestedPermissions='+encodeURIComponent(JSON.stringify(requestedPermissions));
-    
+
+
+    var accessURIc = '&requestedPermissions=' +
+      encodeURIComponent(JSON.stringify(requestedPermissions));
+
     if ((url.length + accessURIc.length) > 2000) {
-      console.log("url too long");
-      url = url + '&poll='+encodeURIComponent(pollURL);
+      console.log('url too long');
+      url = url + '&poll=' + encodeURIComponent(pollURL);
     } else {
       url = url + accessURIc;
     }
-    
-    var accessState = { status: 'NEED_SIGNIN', 
-        code: 201,
-        key: key,
-        requestingAppId: requestingAppId, 
-        requestedPermissions: requestedPermissions, 
-        url: url, 
-        poll: pollURL,
-        returnURL: returnURL,
-        poll_rate_ms: 1000};
-    
-    _setAccessState(res,next,key,accessState);
+
+    var accessState = { status: 'NEED_SIGNIN',
+      code: 201,
+      key: key,
+      requestingAppId: requestingAppId,
+      requestedPermissions: requestedPermissions,
+      url: url,
+      poll: pollURL,
+      returnURL: returnURL,
+      poll_rate_ms: 1000};
+
+    _setAccessState(res, next, key, accessState);
   });
-  
+
   /**
    * polling responder
    */
-  app.get('/access/:key', function(req, res,next) {
-    checkKeyAndGetValue(req,res,next,function(key,value) { 
-      return res.json(value,value.code);
+  app.get('/access/:key', function (req, res, next) {
+    checkKeyAndGetValue(req, res, next, function (key, value) {
+      return res.json(value, value.code);
     });
   });
-  
+
   /**
    * refuse access
    */
-  app.post('/access/:key', function(req, res,next) {
-    checkKeyAndGetValue(req,res,next,function(key,value) { 
-      
-      if (req.body.status == 'REFUSED') {
-        var accessState = { 
-            status: 'REFUSED', 
-            reasonID: req.body.reasonID || 'REASON_UNDEFINED',
-            message:  req.body.message || '',
-            code: 403};
+  app.post('/access/:key', function (req, res, next) {
+    checkKeyAndGetValue(req, res, next, function (key) {
 
-        _setAccessState(res,next,key,accessState);
-      };
-      
-      if (req.body.status == 'ERROR'){
-        var accessState = { 
-            status: 'ERROR', 
-            id: req.body.id || 'INTERNAL_ERROR',
-            message:  req.body.message || '',
-            detail:  req.body.detail || '',
-            code: 403};
-        
-        _setAccessState(res,next,key,accessState);
-      };
-      
-      if (req.body.status == 'ACCEPTED'){
-        
+
+      if (req.body.status === 'REFUSED') {
+        var accessStateA = {
+          status: 'REFUSED',
+          reasonID: req.body.reasonID || 'REASON_UNDEFINED',
+          message:  req.body.message || '',
+          code: 403
+        };
+
+        _setAccessState(res, next, key, accessStateA);
+      }  else if (req.body.status === 'ERROR') {
+        var accessStateB = {
+          status: 'ERROR',
+          id: req.body.id || 'INTERNAL_ERROR',
+          message:  req.body.message || '',
+          detail:  req.body.detail || '',
+          code: 403
+        };
+
+        _setAccessState(res, next, key, accessStateB);
+      } else if (req.body.status === 'ACCEPTED') {
+
         if (! checkAndConstraints.uid(req.body.username)) {
-          return next(messages.e(400,'INVALID_USER_NAME'));
+          return next(messages.e(400, 'INVALID_USER_NAME'));
         }
 
         if (! checkAndConstraints.appToken(req.body.token)) {
-          return next(messages.e(400,'INVALID_DATA'));
+          return next(messages.e(400, 'INVALID_DATA'));
         }
-        
-        var accessState = { 
-            status: 'ACCEPTED',
-            username: req.body.username,
-            token: req.body.token,
-            code: 200};
-        
-        _setAccessState(res,next,key,accessState);
+
+        var accessStateC = {
+          status: 'ACCEPTED',
+          username: req.body.username,
+          token: req.body.token,
+          code: 200
+        };
+
+        _setAccessState(res, next, key, accessStateC);
       }
     });
   });
-  
- 
+
+
 }
 
 
 /**
- * Test the key 
+ * Test the key
  */
-function testKeyAndGetValue(key,success,failed) {
-  if (checkAndConstraints.accesskey(key) == null) {
-    return failed(messages.e(400,'INVALID_KEY'));
+function testKeyAndGetValue(key, success, failed) {
+  if (! checkAndConstraints.accesskey(key)) {
+    return failed(messages.e(400, 'INVALID_KEY'));
   }
-  
-  db.getAccessState(key, function(error, result) {
-    if (error) { return failed(messages.ei(error)) ; }
+
+  db.getAccessState(key, function (error, result) {
+    if (error) { return failed(messages.ei(error)); }
     if (! result) {
-      return failed(messages.e(400,'INVALID_KEY'));
+      return failed(messages.e(400, 'INVALID_KEY'));
     }
 
     success(result);
-  }); 
+  });
 }
 
 /**
  * Check the key in the request and call ifOk() if a value is found
  * @param ifOk callback(value)
  */
-function checkKeyAndGetValue(req,res,next,ifOk) {
-  testKeyAndGetValue(req.params.key,function(value) {
-    ifOk(req.params.key,value);
-  },next);
+function checkKeyAndGetValue(req, res, next, ifOk) {
+  testKeyAndGetValue(req.params.key, function (value) {
+    ifOk(req.params.key, value);
+  }, next);
 }
 
 module.exports = access;
