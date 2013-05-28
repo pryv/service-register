3f5960fcfbedead87b42639d10ec20b3621f4af0
perki <pm@perki.com>
2013-05-28 10:09:19 +0200

    added hosting schema response .. need to find how to deal with refs

--- source/model/schema.responses.js
+++ source/model/schema.responses.js
@@ -1,24 +1,24 @@
 //success : changeEmail
 var success = exports.success = {
-    type: 'object',
-    properties: {
-      success : {
-        type: 'boolean',
-        required: true
-      },
+  type: 'object',
+  properties: {
+    success : {
+      type: 'boolean',
+      required: true
     }
-}
+  }
+};
 
 //route : check
 var checkExists = exports.checkExists = {
-    type: 'object',
-    properties: {
-      exists : {
-        type: 'boolean',
-        required: true
-      }
+  type: 'object',
+  properties: {
+    exists : {
+      type: 'boolean',
+      required: true
     }
-}
+  }
+};
 
 //route : check
 var checkUID = exports.checkUID = {
@@ -29,7 +29,7 @@ var checkUID = exports.checkUID = {
       required: true
     }
   }
-}
+};
 
 //route : admin-users
 var userList = exports.userList = {
@@ -40,7 +40,7 @@ var userList = exports.userList = {
       required: true
     }
   }
-}
+};
 
 
 //route : admin-users
@@ -52,7 +52,7 @@ var serverList = exports.serverList = {
       required: true
     }
   }
-}
+};
 
 
 
@@ -65,155 +65,162 @@ var count = exports.count = {
       required: true
     }
   }
-}
+};
 
 
 //route : init
-var initDone = exports.initDone = {
-    type: 'object',
-    properties: {
-      id: {
-        type: 'string',
-        required: true
-      },
-      message: {
-        type: 'string',
-        required: true
-      },
-      detail: {
-        type: 'string',
-        required: true
-      },
-      captchaChallenge : {
-        type: 'string',
-        required: true
-      }
+var userCreated = exports.userCreated = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    },
+    detail: {
+      type: 'string',
+      required: true
     }
-}
+  }
+};
 
 
 //confirm : server & server : server
 var server = exports.server = {
-    type: 'object',
-    properties: {
-      server : {
-        type: 'string',
-        required: true
-      },
-      alias : {
-        type: 'string',
-        required: true
-      }
+  type: 'object',
+  properties: {
+    server : {
+      type: 'string',
+      required: true
+    },
+    alias : {
+      type: 'string',
+      required: true
     }
-}
+  }
+};
 
 //confirm : server (already confirmed)
 var alreadyConfirmed = exports.alreadyConfirmed = {
-    type: 'object',
-    properties: {
-      server : {
-        type: 'string',
-        required: true
-      },
-      alias : {
-        type: 'string',
-        required: true
-      },
-      id: {
-        type: 'string',
-        required: true
-      },
-      message: {
-        type: 'string',
-        required: true
-      },
-      detail: {
-        type: 'string',
-        required: true
-      }
+  type: 'object',
+  properties: {
+    server : {
+      type: 'string',
+      required: true
+    },
+    alias : {
+      type: 'string',
+      required: true
+    },
+    id: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    },
+    detail: {
+      type: 'string',
+      required: true
     }
-}
+  }
+};
+
+//hostings
+var hostings = exports.hostings = {
+  type: 'object',
+  properties: {
+    regions: {
+      type: 'object'
+      // TODO add references to
+    }
+  }
+};
 
 
 //errors
 var error = exports.error = {
-    type: 'object',
-    properties: {
-      id: {
-        type: 'string',
-        required: true
-      },
-      message: {
-        type: 'string',
-        required: true
-      },
-      detail: {
-        type: 'string',
-        required: true
-      }
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    },
+    detail: {
+      type: 'string',
+      required: true
     }
+  }
 };
 
 //errors multiple
 var multipleErrors = exports.multipleErrors = {
-    type: 'object',
-    properties: {
-      id: {
-        type: 'string',
-        required: true
-      },
-      message: {
-        type: 'string',
-        required: true
-      },
-      detail: {
-        type: 'string',
-        required: true
-      },
-      errors : {
-        type : 'object',
-        items : {
-          properties : {
-            id: {
-              type: 'string',
-              required: true
-            },
-            message: {
-              type: 'string',
-              required: true
-            },
-            detail: {
-              type: 'string',
-              required: true
-            }
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      required: true
+    },
+    message: {
+      type: 'string',
+      required: true
+    },
+    detail: {
+      type: 'string',
+      required: true
+    },
+    errors : {
+      type : 'object',
+      items : {
+        properties : {
+          id: {
+            type: 'string',
+            required: true
+          },
+          message: {
+            type: 'string',
+            required: true
+          },
+          detail: {
+            type: 'string',
+            required: true
           }
         }
       }
     }
+  }
 };
 
 
 
 var accessPOST = exports.accessPOST = {
-    type: 'object',
-    properties: {
-      url : {
-        type: 'string', // url?
-        required: true
-      },
-      poll : {
-        type: 'string', // url?
-        required: true
-      }
+  type: 'object',
+  properties: {
+    url : {
+      type: 'string', // url?
+      required: true
+    },
+    poll : {
+      type: 'string', // url?
+      required: true
     }
-}
+  }
+};
 
 var accessGET = exports.accessGET = {
-    type: 'object',
-    properties: {
-      status : {
-        type: 'string', 
-        required: true
-      }
+  type: 'object',
+  properties: {
+    status : {
+      type: 'string',
+      required: true
     }
-}
+  }
+};
