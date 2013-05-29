bc5877cd426e70133d5365e7faa9de6fb987e9e5
perki <pm@perki.com>
2013-05-29 22:27:51 +0200

    JSHint and bringing back http mode

--- source/server.js
+++ source/server.js
@@ -8,37 +8,48 @@ fs = require('fs'),
 logger = require('winston'),
 _ = require('underscore.string');
 
-
 var ready = require('readyness');
 ready.setLogger(logger.info);
 
 
 //https server
-logger.info('Register  server :'+config.get('http:register:url'));
-logger.info('Static  server :'+config.get('http:static:url'));
+logger.info('Register  server :' + config.get('http:register:url'));
+logger.info('Static  server :' + config.get('http:static:url'));
 
 
 if (config.get('server:port') > 0) {
 
-  var serverOptions = {
-    key : fs.readFileSync(config.get('server:certsPathAndKey')+'-key.pem').toString(),
-    cert : fs.readFileSync(config.get('server:certsPathAndKey')+'-cert.crt').toString(),
-    ca : fs.readFileSync(config.get('server:certsPathAndKey')+'-ca.pem').toString(),
-  };
+  var serverOptions = {};
 
+  var server = null;
+  if (config.get('server:ssl')) {
+    serverOptions = {
+      key : fs.readFileSync(config.get('server:certsPathAndKey') + '-key.pem').toString(),
+      cert : fs.readFileSync(config.get('server:certsPathAndKey') + '-cert.crt').toString(),
+      ca : fs.readFileSync(config.get('server:certsPathAndKey') + '-ca.pem').toString()
+    };
+    server =  require('https').createServer(serverOptions, app);
+  } else {
+    server =  require('http').createServer(app);
+  }
 
-  var server = require('https').createServer(serverOptions, app);
-  var appListening = ready.waitFor('register:listening:'+config.get('server:ip')+':'+config.get('server:port'));
-  server.listen(config.get('server:port'),config.get('server:ip'), function() {
+  var appListening = ready.waitFor('register:listening:' + config.get('server:ip') +
+    ':' + config.get('server:port'));
+  server.listen(config.get('server:port'), config.get('server:ip'), function () {
     var address = server.address();
     var protocol = server.key ? 'https' : 'http';
-    appListening(_.sprintf('SW %s://%s:%d in %s mode with ssl certs: '+config.get('server:certsPathAndKey'),
+
+    var sslmessage =  server.key ? ' with ssl certs: ' +
+    config.get('server:certsPathAndKey') : '';
+
+    appListening(_.sprintf('SW %s://%s:%d in %s mode ' + sslmessage,
       protocol, address.address, address.port, app.settings.env));
-  }).on('error',function (e) {
-      if (e.code == 'EACCES') {
-        logger.error('Cannot '+e.syscall+' on: '+ip+':'+port);
-        throw(e);
-      }});
+  }).on('error', function (e) {
+      if (e.code === 'EACCES') {
+        logger.error('Cannot ' + e.syscall);
+        throw (e);
+      }
+    });
 
   module.exports = server;
 } else {
--- test/load/check.js
+++ test/load/check.js
@@ -1,34 +1,40 @@
-var app = require('../../source/app');
+/*global describe,it*/
+require('../../source/server');
 var config = require('../../source/utils/config');
-var mode = config.get('http:server:ssl') ? 'https' : 'http';
-var http = require(mode); 
+var mode = config.get('server:ssl') ? 'https' : 'http';
+var http = require(mode);
+
+require('readyness/wait/mocha');
 
 //TODO Data validation
 
-describe('LOAD ', function(){
+describe('LOAD ', function () {
   it('GET /:uid/check  10000x', function(done){
 
     var requests = 5000;
     var count = requests + 0;
 
 
-    function gotit(res,done) {
+    function gotit(res, done) {
       count--;
-      if (count == 0) done();
+      if (count === 0) { done(); }
     }
 
-
-    var http_options = { path: '/perki/check', port: config.get('http:register:port')};
-
-    for (var i = 0; i < requests; i++) {
+    function charge() {
       //console.log(JSON.stringify(test));
-      var req = http.request(http_options, function(res){
-        gotit(res,done);
+      var req = http.request(http_options, function (res) {
+        gotit(res, done);
       }).on('error', function(e) {
-        throw new Error("Got error: " + e.message, e);
-      });
+          throw new Error('Got error: ' + e.message, e);
+        });
 
       req.end();
+    }
+
+    var http_options = { path: '/perki/check_username', port: config.get('server:port')};
+    require('../../source/utils/dump').inspect({mode: mode, http_options: http_options});
+    for (var i = 0; i < requests; i++) {
+      charge();
     }
   });
 
