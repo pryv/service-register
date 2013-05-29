d553cdc573fe0e1cdf5af78f6eb43765cecd4ca5
perki <pm@perki.com>
2013-05-29 15:24:56 +0200

    JSHint cleanup

--- source/storage/user-management.js
+++ source/storage/user-management.js
@@ -4,34 +4,32 @@
 
 var db = require('../storage/database.js');
 
-
 var exports = exports || {};
 
 
-
 /**
- * @param callback function(error,json of {serversName : usage})
+ * @param callback function(error, json of {serversName : usage})
  */
 exports.getServers = function getServers(callback) {
   var result = {};
-  db.doOnKeysValuesMatching('*:server','*',
-    function (key,value) {
-      if (typeof(result[value]) == 'undefined') result[value] = 0;
-      result[value]++
+  db.doOnKeysValuesMatching('*:server', '*',
+    function (key, value) {
+      if (typeof(result[value]) === 'undefined') { result[value] = 0; }
+      result[value]++;
     },
     function (error) {
-      callback(error,result);
+      callback(error, result);
     });
-}
+};
 
 
 /**
  *
  * @param serverName
- * @param callback function(error,array of users)
+ * @param callback function(error, array of users)
  */
-exports.getUsersOnServer = function getUsersOnServer(serverName,callback) {
-  var result = new Array();
+exports.getUsersOnServer = function getUsersOnServer(serverName, callback) {
+  var result = [];
   db.doOnKeysValuesMatching('*:server', serverName,
     function (key) {
       result.push(key.split(':')[0]);
@@ -39,18 +37,18 @@ exports.getUsersOnServer = function getUsersOnServer(serverName,callback) {
     function (error) {
       callback(error, result);
     });
-}
+};
 
 
 /**
  *
  * @param srcServerName
  * @param dstServerName
- * @param callback function(error,number_of_changes)
+ * @param callback function(error, number_of_changes)
  */
 exports.renameServer = function renameServer(srcServerName, dstServerName, callback) {
 
-  var errors = new Array();
+  var errors = [];
 
   var receivedCount = 0;
   var actionThrown = 0;
@@ -71,17 +69,17 @@ exports.renameServer = function renameServer(srcServerName, dstServerName, callb
 
 
   db.doOnKeysValuesMatching('*:server', srcServerName,
-    function (key, value) {
+    function (key) {
       var uid = key.split(':')[0];
       actionThrown++;
       checkDone();
-      db.setServer(uid,dstServerName,function(error,result) {
-         if (error) {
-           errors.push(error);
-         }
+      db.setServer(uid, dstServerName, function (error) {
+        if (error) {
+          errors.push(error);
+        }
         receivedCount++;
         checkDone();
       });
 
-    },myDone);
-}
+    }, myDone);
+};
