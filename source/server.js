5473e4b0cc855b7480f13eed821d54d9f8b9a176
Perki <pm@perki.com>
2013-11-08 16:06:44 +0100

    newrelic integration

--- source/server.js
+++ source/server.js
@@ -12,6 +12,13 @@ var ready = require('readyness');
 ready.setLogger(logger.info);
 
 
+// -- new relic -- hack conf in order to pass directly configuration values
+process.env.NEW_RELIC_NO_CONFIG_FILE = true;
+process.env.NEW_RELIC_LICENSE_KEY = config.get('newrelic:license_key');
+process.env.NEW_RELIC_APP_NAME = config.get('newrelic:app_name');
+require('newrelic');
+
+
 //https server
 logger.info('Register  server :' + config.get('http:register:url'));
 logger.info('Static  server :' + config.get('http:static:url'));
