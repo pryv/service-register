b2998a2bcab6dbdca9ccfd0bcb70b594138d77f3
Artur Skonecki <skoneka@users.noreply.github.com>
2015-07-06 20:29:16 +0200

    quickfix: add support for multiple domains (hack)

--- source/app-dns.js
+++ source/app-dns.js
@@ -67,9 +67,8 @@ var serverForName = function (reqName, callback, req, res) {
   }
 
   logger.info('DNS: ' + keyName);
-
-  // root request
-  if (keyName === config.get('dns:domain')) {
+ 
+  if ( config.get('dns:domains').indexOf(keyName) ) { 
     switch (req.q[0].typeName) {
     case 'MX':
       return callback(req, res, dns.getRecords(mxData, reqName));
@@ -80,14 +79,18 @@ var serverForName = function (reqName, callback, req, res) {
       return callback(req, res, dns.getRecords(soaData, reqName));
     default:
       return callback(req, res, dns.getRecords(rootData, reqName));
-    }
+    }   
   }
-
 
   console.log("**** " + keyName);
 
   // look for matches within domain .pryv.io
-  var uid = checkAndConstraints.extractRessourceFromHostname(keyName);
+  try {
+    var uid = checkAndConstraints.extractRessourceFromHostname(keyName);
+  }
+  catch (err) {
+    logger.info('DNS: ' + err);
+  }
 
   if (! uid) {
     logger.info('DNS: (Not in domain) ' + keyName);
--- source/utils/check-and-constraints.js
+++ source/utils/check-and-constraints.js
@@ -8,42 +8,42 @@ _.str = require('underscore.string');
 _.mixin(_.str.exports());
 
 var checkUsername = new RegExp('^' + '([a-z0-9-]{1,100})' + '$');
-
-/**
- * extract resource and domain name from hostname
- */
-
-exports.extractDomainAndResource = function (hostname) {
-    config_get_dns_domains = ["pryv.in","pryv.li"];
-    domains = config_get_dns_domains;
-
-    for (var i = 0; i < domains.length; i++) {
-       if ( hostname.endsWith('.' + domains[i]) ) {
-        resource = hostname.slice(0, - domains[i].length - 1 )
-        if (checkUsername.exec(resource)) {
-            return { resource: resource, domain: domains[i]};
-          }
-        else {
-            throw new Error("Username not recognized in hostname.")
-          }
-       }
-    }
-    throw new Error("Domain name not recognized in hostname.");
-}
+var checkDomain = new RegExp('([.a-z0-9-]{1,100})' + '$');
 
 
+String.prototype.endsWith = function(suffix) {
+      return this.indexOf(suffix, this.length - suffix.length) !== -1; 
+};
 
 /**
  * extract resource from hostname
  */
 exports.extractRessourceFromHostname = function (hostname) {
-  var matchArray = extractRessourceFromHostnameRegExp.exec(hostname);
-  if (! matchArray) { return null; }
-  return matchArray[1];
-};
+    domains = config.get('dns:domains');
 
-var _temp = '([a-z0-9-]{1,100})\\.' + config.get('dns:domain').replace(/\./g, '\\.');
-var extractRessourceFromHostnameRegExp = new RegExp('^' + _temp + '$');
+    Object.prototype.getName = function() { 
+         var funcNameRegex = /function (.{1,})\(/;
+            var results = (funcNameRegex).exec((this).constructor.toString());
+               return (results && results.length > 1) ? results[1] : ""; 
+    }   
+
+    console.log(hostname);
+    console.log(hostname.getName());
+
+    for (var i = 0; i < domains.length; i++) {
+       if ( hostname.endsWith('.' + domains[i]) ) { 
+        resource = hostname.slice(0, - domains[i].length - 1 ) 
+        if (checkUsername.exec(resource)) {
+            //return { resource: resource, domain: domains[i]};
+            return resource;
+          }   
+        else {
+            throw new Error("Username not recognized in hostname.");
+          }   
+       }   
+    }   
+    throw new Error("Domain name not recognized in hostname.");
+}
 
 // (alphanumeric between 5 an 21 chars) case-insensitive  -  authorized
 // trim the uid ..
