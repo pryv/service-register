54298c87c9da71841c7e935b8451fde0c220415a
Thiebaud <tmodoux@pryv.com>
2016-12-08 11:39:39 +0100

    Fix missing dns port

--- test/config-dns-test.js
+++ test/config-dns-test.js
@@ -2,5 +2,6 @@ var config = require('../source/utils/config');
 
 config.set('dns:ip', '127.0.0.1');
 config.set('dns:name', 'localhost');
+config.set('dns:port', '2053');
 
 module.exports = config;
