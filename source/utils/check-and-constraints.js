f83c0fa2118077198dc3ce72e4cdb0f06d729a73
Thiebaud <tmodoux@pryv.com>
2017-02-09 15:15:08 +0100

    Differentiate export only functions

--- source/utils/check-and-constraints.js
+++ source/utils/check-and-constraints.js
@@ -1,15 +1,12 @@
 /**
  * Tools to perform test and minimum cleaning on inputs
  */
-var config = require('./config');
-
-var  _ = require('underscore');
+var config = require('./config'),
+  _ = require('underscore');
 _.str = require('underscore.string');
 _.mixin(_.str.exports());
 
 var checkUsername = new RegExp('^' + '([a-z0-9-]{1,100})' + '$');
-//var checkDomain = new RegExp('([.a-z0-9-]{1,100})' + '$');
-
 
 String.prototype.endsWith = function(suffix) {
       return this.indexOf(suffix, this.length - suffix.length) !== -1;
@@ -43,7 +40,7 @@ exports.extractRessourceFromHostname = function (hostname) {
 
 // (alphanumeric between 5 an 21 chars) case-insensitive  -  authorized
 // trim the uid ..
-exports.uid = function uid(str) {
+exports.uid = function (str) {
   if (! str) { return null; }
   str = _(str).trim().toLowerCase();
   var filter = /^([a-zA-Z0-9])(([a-zA-Z0-9\-]){3,100})([a-zA-Z0-9])$/;
@@ -53,37 +50,35 @@ exports.uid = function uid(str) {
 
 // (alphanumeric between 2 an 70 chars) case-insensitive  - and . authorized
 // trim the hosting ..
-exports.hosting = function hosting(str) {
+exports.hosting = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   var filter =  /^([a-zA-Z0-9])(([a-zA-Z0-9\-\.]){2,70})([a-zA-Z0-9])$/;
   return (filter.test(str)) ? str : null;
 };
 
-
-
 // any chars between 6 and 99 chars, with no trailing spaces.
-exports.password = function password(str) {
+exports.password = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   return (str.length > 5 && str.length < 100) ? str : null;
 };
 
 // any chars between 5 and 99 chars, with no trailing spaces.
-exports.invitationToken = function invitationToken(str) {
+exports.invitationToken = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   return (str.length > 4 && str.length < 100) ? str : null;
 };
 
 // any chars between 1 and 99 chars, with no trailing spaces.
-exports.referer = function referer(str) {
+exports.referer = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   return (str.length > 0 && str.length < 100) ? str : null;
 };
 
-exports.email = function email(str) {
+exports.email = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   // not perfect 
@@ -91,7 +86,7 @@ exports.email = function email(str) {
   return (filter.test(str)) ? str : null;
 };
 
-exports.challenge = function challenge(str) {
+exports.challenge = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   var filter = /^([a-zA-Z0-9]{5,200})$/;
@@ -103,7 +98,7 @@ exports.challenge = function challenge(str) {
  * @param str
  * @return {*}
  */
-exports.hostname = function hostname(str) {
+exports.hostname = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   var filter = /^([a-zA-Z0-9_\.\-]{3,256})$/;
@@ -111,45 +106,45 @@ exports.hostname = function hostname(str) {
 };
 
 var supportedLanguages = {en : 'English', fr: 'Français'};
-exports.lang = function lang(str) {
+exports.lang = function (str) {
   if (! str) { return 'en'; }
   return (supportedLanguages.hasOwnProperty(str)) ? str : 'en';
 };
 
 
-exports.appID = function appID(str) {
+exports.appID = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   return (str.length > 5 && str.length < 100) ? str : null;
 };
 
 
-exports.activitySessionID = function activitySessionID(str) {
+exports.activitySessionID = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   return (str.length > 5 && str.length < 100) ? str : null;
 };
 
-exports.appAuthorization = function appAuthorization(str) {
+exports.appAuthorization = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   var filter = /^([a-zA-Z0-9]{10,200})$/;
   return (filter.test(str)) ? str : null;
 };
 
-exports.appToken = function appToken(str) {
+exports.appToken = function (str) {
   if (! str) { return null; }
   return (str.length < 256) ? str : null;
 };
 
-exports.accesskey = function accessKey(str) {
+exports.accesskey = function (str) {
   if (! str) { return null; }
   str = _(str).trim();
   var filter = /^([a-zA-Z0-9]{10,200})$/;
   return (filter.test(str)) ? str : null;
 };
 
-exports.access = function access(json) {
+exports.access = function (json) {
   //TODO Check access structure
   return json;
 };
