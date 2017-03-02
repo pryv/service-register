36c1925a28b1c8b6409f2cc8b034fb2d69fd4508
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2017-03-02 11:48:35 +0100

    Create build source code before launching docker
    This is cleaner, since the docker image will never contain the real source
    code in the first place.

--- build/register/release.sh
+++ build/register/release.sh
@@ -18,9 +18,6 @@ run run tar -x --owner app -f \
 
 PYTHON=$(which python2.7) run yarn install --production
 
-# Copy all source code to 'lib', removing flow annotations.
-run npm run release && rm -r ./source && mv ./lib ./source
-
 # Copy the config file
 run mkdir -p $conf_dir && \
   cp /pd_build/config/register.json $conf_dir/register.json
