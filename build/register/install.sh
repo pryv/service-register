baa3596977c74b30bf8892bb22afec698852e3ad
Thiébaud Modoux <tmodoux@pryv.com>
2018-03-01 14:30:59 +0100

    Use rm method for removing  optional services

--- build/register/install.sh
+++ build/register/install.sh
@@ -5,5 +5,9 @@ source /pd_build/buildconfig
 # Install the service-register application.
 run /pd_build/release.sh
 
+# Remove cron and sshd entirely, unless we use them
+run rm -r /etc/service/cron
+run rm -r /etc/service/sshd && rm /etc/my_init.d/00_regen_ssh_host_keys.sh
+
 # Clean up after ourselves.
 run /pd_build/finalize.sh
