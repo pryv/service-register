24f26a06008b05df6f377ee6049e5d95cf5ed453
Thiébaud Modoux <tmodoux@pryv.com>
2018-03-07 16:22:18 +0100

    Debugging

--- source/dns/ndns-wrapper.js
+++ source/dns/ndns-wrapper.js
@@ -67,7 +67,7 @@ type DnsDynamicHandler = (
 
 // Handles each individual DNS request - our main handler function. 
 function onDnsRequest(dynamic_call: DnsDynamicHandler, req: any, res: any) {
-  logger.info('REQ: ', req);
+  logger.info(JSON.stringify(req.q));
   if (req.q.length > 0) {
     const name = validateRequest(req);
     return dynamic_call(name, sendResponse, req, res);
