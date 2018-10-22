e08a4990af829e78d1f35e93a47080ccfd6a2bea
Ilia Kebets <kebetsi@gmail.com>
2018-10-22 15:38:21 +0200

    reorganize tests: remove other routes from "POST /user" as they do not share neither path nor HTTP method

--- test/acceptance/users.test.js
+++ test/acceptance/users.test.js
@@ -345,237 +345,237 @@ describe('POST /user', function () {
         });
     });
   });
+});
 
 
-  describe('POST /username/check', function () {
-    var path = '/username/check';
-    const defaults = {
-      hosting: 'test.ch-ch',
-      appid: 'pryv-test',
-      username: randomuser(),
-      email: randomuser() + '@wactiv.chx', // should not be necessary
-      password: 'abcdefgh',
-      invitationtoken: 'enjoy',
-      referer: 'pryv'
+describe('POST /username/check', function () {
+  var path = '/username/check';
+  const defaults = {
+    hosting: 'test.ch-ch',
+    appid: 'pryv-test',
+    username: randomuser(),
+    email: randomuser() + '@wactiv.chx', // should not be necessary
+    password: 'abcdefgh',
+    invitationtoken: 'enjoy',
+    referer: 'pryv'
+  };
+
+  it('reserved list', function (done) {
+    var test = {username: 'facebook', status: 200, desc: 'reserved from list',
+      value: 'false', restype:'text/plain'};
+
+    request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('reserved pryv', function (done) {
+    var test = {username: 'pryvtoto', status: 200, desc: 'reserved for pryv',
+      value: 'false', restype:'text/plain'};
+
+    request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('available', function (done) {
+    var test = {username: 'asdfhgsdkfewg', status: 200, desc: 'available',
+      value: 'true', restype:'text/plain'};
+
+    request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+});
+
+describe('GET /:username/check_username', function () {
+
+  function getPath(username) {
+    return '/' + (username || defaultUsername) + '/check_username';
+  }
+
+  it('too short', function (done) {
+    var test = {
+      username: 'abcd', status: 400, desc: 'too short ',
+      JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
     };
 
-    it('reserved list', function (done) {
-      var test = {username: 'facebook', status: 200, desc: 'reserved from list',
-        value: 'false', restype:'text/plain'};
-
-      request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
     });
-
-    it('reserved pryv', function (done) {
-      var test = {username: 'pryvtoto', status: 200, desc: 'reserved for pryv',
-        value: 'false', restype:'text/plain'};
-
-      request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('available', function (done) {
-      var test = {username: 'asdfhgsdkfewg', status: 200, desc: 'available',
-        value: 'true', restype:'text/plain'};
-
-      request.post(server.url + path).send(_.extend(defaults, test)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
   });
 
-  describe('GET /:username/check_username', function () {
+  it('invalid username', function (done) {
+    var test = {
+      username: 'abcdefghijklmnopqrstuvwxyzasaasaaas' +
+      'abcdefghijklmnopqrstuvwxyzasaasaaas' +
+      'abcdefghijklmnopqrstuvwxyzasaasaaas' +
+      'abcdefghijklmnopqrstuvwxyzasaasaaas', status: 400, desc: 'too long ',
+      JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
+    };
 
-    function getPath(username) {
-      return '/' + (username || defaultUsername) + '/check_username';
-    }
-
-    it('too short', function (done) {
-      var test = {
-        username: 'abcd', status: 400, desc: 'too short ',
-        JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
     });
-
-    it('invalid username', function (done) {
-      var test = {
-        username: 'abcdefghijklmnopqrstuvwxyzasaasaaas' +
-        'abcdefghijklmnopqrstuvwxyzasaasaaas' +
-        'abcdefghijklmnopqrstuvwxyzasaasaaas' +
-        'abcdefghijklmnopqrstuvwxyzasaasaaas', status: 400, desc: 'too long ',
-        JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('invalid character 1', function (done) {
-      var test = {
-        username: 'abc%20def', status: 400, desc: 'invalid character 1',
-        JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('invalid character 2', function (done) {
-      var test = {
-        username: 'abc.def', status: 400, desc: 'invalid character 2',
-        JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('authorized', function (done) {
-      var test = {
-        username: 'abcd-ef', status: 200, desc: '- authorized ',
-        JSchema: schemas.checkUID
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('correct', function (done) {
-      var test = {
-        username: 'wactiv', status: 200, desc: 'correct ',
-        JSchema: schemas.checkUID
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('always available', function (done) {
-      var test = {
-        username: 'recla', status: 200, desc: 'always available ',
-        JSchema: schemas.checkUID
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('reserved for pryv', function (done) {
-      var test = {
-        username: 'pryvtoto', status: 200, desc: 'reserved for pryv',
-        JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('reserved from list', function (done) {
-      var test = {
-        username: 'facebook', status: 200, desc: 'reserved from list',
-        JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
-    it('reserved dns', function (done) {
-      var test = {
-        username: 'access', status: 200, desc: 'reserved dns',
-        JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
-      };
-
-      request.get(server.url + getPath(test.username)).end(function (err, res) {
-        dataValidation.jsonResponse(err, res, test, done);
-      });
-    });
-
   });
 
-  describe('POST /users/:username/change-email', function () {
+  it('invalid character 1', function (done) {
+    var test = {
+      username: 'abc%20def', status: 400, desc: 'invalid character 1',
+      JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
+    };
 
-    function getPath(username) {
-      return '/users/' + (username || defaultUsername) + '/change-email';
-    }
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
 
-    it('must change the username\'s email', function (done) {
-      request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
-        .set('Authorization', defaultAuth)
-        .end((err, res) => {
-          dataValidation.check(res, {
-            status: 200,
-            schema: schemas.success,
-            body: {success: true}
-          }, done);
-        });
-    });
-    it('must return an error if the username is unknown', function (done) {
-      request.post(server.url + getPath('baduser')).send({email: 'toto@pryv.io'})
-        .set('Authorization', defaultAuth)
-        .end((err, res) => {
-          dataValidation.checkError(res, {
-            status: 404,
-            id: 'UNKNOWN_USER_NAME'
-          }, done);
-        });
-    });
-    it('must return an error if the email is invalid', function (done) {
-      request.post(server.url + getPath()).send({email: 'bad@email'})
-        .set('Authorization', defaultAuth)
-        .end((err, res) => {
-          dataValidation.checkError(res, {
-            status: 400,
-            id: 'INVALID_EMAIL'
-          }, done);
-        });
-    });
-    it('must return an error if the request auth key is missing or unknown', function (done) {
-      request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
-        .end((err, res) => {
-          dataValidation.checkError(res, {
-            status: 401,
-            'id': 'unauthorized'
-          }, done);
-        });
-    });
-    it('must return an error if the request auth key is unauthorized', function (done) {
-      request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
-        .set('Authorization', 'test-admin-key')
-        .end((err, res) => {
-          dataValidation.checkError(res, {
-            status: 403,
-            'id': 'forbidden'
-          }, done);
-        });
-    });
+  it('invalid character 2', function (done) {
+    var test = {
+      username: 'abc.def', status: 400, desc: 'invalid character 2',
+      JSchema: schemas.error, JValues: {'id': 'INVALID_USER_NAME'}
+    };
 
-    after(function (done) {
-      // reset test user (could be optimized by directly calling into the DB)
-      request.post(server.url + getPath()).send({email: defaultEmail})
-        .set('Authorization', defaultAuth)
-        .end((err, res) => {
-          dataValidation.check(res, {
-            status: 200,
-            schema: schemas.success
-          }, done);
-        });
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
     });
+  });
+
+  it('authorized', function (done) {
+    var test = {
+      username: 'abcd-ef', status: 200, desc: '- authorized ',
+      JSchema: schemas.checkUID
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('correct', function (done) {
+    var test = {
+      username: 'wactiv', status: 200, desc: 'correct ',
+      JSchema: schemas.checkUID
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('always available', function (done) {
+    var test = {
+      username: 'recla', status: 200, desc: 'always available ',
+      JSchema: schemas.checkUID
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('reserved for pryv', function (done) {
+    var test = {
+      username: 'pryvtoto', status: 200, desc: 'reserved for pryv',
+      JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('reserved from list', function (done) {
+    var test = {
+      username: 'facebook', status: 200, desc: 'reserved from list',
+      JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+  it('reserved dns', function (done) {
+    var test = {
+      username: 'access', status: 200, desc: 'reserved dns',
+      JSchema: schemas.checkUID, JValues: {reserved: true, reason: 'RESERVED_USER_NAME'}
+    };
+
+    request.get(server.url + getPath(test.username)).end(function (err, res) {
+      dataValidation.jsonResponse(err, res, test, done);
+    });
+  });
+
+});
+
+describe('POST /users/:username/change-email', function () {
+
+  function getPath(username) {
+    return '/users/' + (username || defaultUsername) + '/change-email';
+  }
+
+  it('must change the username\'s email', function (done) {
+    request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
+      .set('Authorization', defaultAuth)
+      .end((err, res) => {
+        dataValidation.check(res, {
+          status: 200,
+          schema: schemas.success,
+          body: {success: true}
+        }, done);
+      });
+  });
+  it('must return an error if the username is unknown', function (done) {
+    request.post(server.url + getPath('baduser')).send({email: 'toto@pryv.io'})
+      .set('Authorization', defaultAuth)
+      .end((err, res) => {
+        dataValidation.checkError(res, {
+          status: 404,
+          id: 'UNKNOWN_USER_NAME'
+        }, done);
+      });
+  });
+  it('must return an error if the email is invalid', function (done) {
+    request.post(server.url + getPath()).send({email: 'bad@email'})
+      .set('Authorization', defaultAuth)
+      .end((err, res) => {
+        dataValidation.checkError(res, {
+          status: 400,
+          id: 'INVALID_EMAIL'
+        }, done);
+      });
+  });
+  it('must return an error if the request auth key is missing or unknown', function (done) {
+    request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
+      .end((err, res) => {
+        dataValidation.checkError(res, {
+          status: 401,
+          'id': 'unauthorized'
+        }, done);
+      });
+  });
+  it('must return an error if the request auth key is unauthorized', function (done) {
+    request.post(server.url + getPath()).send({email: 'toto@pryv.io'})
+      .set('Authorization', 'test-admin-key')
+      .end((err, res) => {
+        dataValidation.checkError(res, {
+          status: 403,
+          'id': 'forbidden'
+        }, done);
+      });
+  });
+
+  after(function (done) {
+    // reset test user (could be optimized by directly calling into the DB)
+    request.post(server.url + getPath()).send({email: defaultEmail})
+      .set('Authorization', defaultAuth)
+      .end((err, res) => {
+        dataValidation.check(res, {
+          status: 200,
+          schema: schemas.success
+        }, done);
+      });
   });
 });
