226fb9d0997611ba8347ede10cbe465c5ce1baa6
Kaspar Schiess <kaspar@pryv.com>
2018-10-29 09:33:52 +0100

    Flowify app-errors; fix signature

--- source/middleware/app-errors.js
+++ source/middleware/app-errors.js
@@ -1,4 +1,4 @@
-/*jshint -W098*/
+//@flow
 
 var logger = require('winston');
 var messages = require('./../utils/messages');
@@ -6,8 +6,8 @@ var messages = require('./../utils/messages');
 /**
  * Error middleware, may be used for user management
  */
-function app_errors(app) {
-  app.use(function (error, req, res, next) {
+function app_errors(app: express$Application) {
+  app.use(function (error, req: express$Request, res, next) { // eslint-disable-line no-unused-vars
     if (error instanceof messages.REGError) {
       //logger.debug('app_errors : '+ JSON.stringify(error.data));
       return res.status(error.httpCode).json(error.data);
@@ -17,22 +17,12 @@ function app_errors(app) {
       logger.error('app_errors unknown object : ' + error);
       logger.error((new Error()).stack);
     }  else {
-      logger.error('app_errors : ' + error);
+      logger.error('app_errors : ' + error.toString());
       logger.error(error.stack);
     }
     var err = new messages.REGError(500, messages.say('INTERNAL_ERROR'));
     res.status(err.httpCode).json(err.data);
   });
-
-  /** We let this to airbrake
-  process.on('uncaughtException', function (err) {
-    if (! err) {
-      err = new Error();
-    }
-    logger.error('uncaughtException : ' + err);
-    logger.error(err.stack);
-  });
-  **/
 }
 
 module.exports = app_errors;
