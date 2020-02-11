cfa10b907fd4f0358096706266d2d2b8d9ffa706
Ilia Kebets <kebetsi@gmail.com>
2020-02-11 18:27:49 +0100

    refactor bin/server

---
+++ bin/server.js
@@ -0,0 +1,15 @@
+const config = require('../source/utils/config');
+const Server = require('../source/server');
+const logger = require('winston');
+
+(async () => {
+  const server = new Server(config);
+  await server.start();
+  logger.info('Startup sequence complete, Server is running.');
+})()
+  .then(() => logger.info('Startup sequence complete, Server is running.'))
+  .catch(e => {
+    if (logger) logger.error(e);
+    console.dir(e);
+    process.exit(1);
+  });
