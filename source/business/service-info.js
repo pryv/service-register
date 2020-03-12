a5283d72f6789e664db248bca8e04b5216d9f534
Ilia Kebets <kebetsi@gmail.com>
2020-03-12 17:42:48 +0100

    Revert "generalize addition of trailing "/" in service info"
    This reverts commit dac4077fb6632f50cc05351aabee59646a238a7b.

--- source/business/service-info.js
+++ source/business/service-info.js
@@ -1,17 +1,15 @@
 const config = require('../config');
-const checkAndConstraints = require('../utils/check-and-constraints');
+const url = require('url');
 
 const info = Object.assign({}, config.get('service'));
 
 info.serial = config.get('serial');
 
 
-// add eventual trailing '/' to all URL fields of service information;
-Object.keys(info).forEach(k => {
-  if (checkAndConstraints.url(info[k])) {
-    if (info[k].slice(-1) !== '/') {
-      info[k] += '/';
-    }
+// add eventual missing '/';
+['access', 'api', 'register'].forEach((key) => { 
+  if (info[key].slice(-1) !== '/') {
+    info[key] += '/';
   }
 });
 
