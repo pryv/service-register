7f86d9f5b6e4871b6abfb02d321bfd1e10c5570b
Perki <pm@perki.com>
2020-03-12 11:42:29 +0100

    Moved serial to service-info

--- source/utils/service-info.js
+++ source/utils/service-info.js
@@ -3,9 +3,6 @@ const url = require('url');
 
 const info = Object.assign({}, config.get('service'));
 
-info.serial = config.get('serial');
-
-
 // add eventual missing '/';
 ['access', 'api', 'register'].forEach((key) => { 
   if (info[key].slice(-1) !== '/') {
