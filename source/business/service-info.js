00a924e71b4a40a989ee30d1fcfc60ee02d1a7f1
Ilia Kebets <kebetsi@gmail.com>
2020-10-05 16:37:30 +0200

    add stub value for open-source

--- source/business/service-info.js
+++ source/business/service-info.js
@@ -7,10 +7,16 @@
 const config = require('../config');
 const url = require('url');
 
+const STUB_VALUE_FOR_OPEN_SOURCE = '1.0.0';
+
 // get version from the file that is in the container
 const info = Object.assign({}, config.get('service'));
 const reportingSettings = config.get('reporting');
-info['version'] = '1.0.0';
+if (reportingSettings == null) {
+  info['version'] = STUB_VALUE_FOR_OPEN_SOURCE;
+} else {
+  info['version'] = reportingSettings.templateVersion;
+}
 
 // add eventual missing '/';
 ['access', 'api', 'register'].forEach((key) => {
